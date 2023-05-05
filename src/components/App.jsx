8212875aba080d954abd403ad5d9f94498d3ae5e
YurionStyle <74459224+YurionStyle@users.noreply.github.com>
2023-05-05 22:22:56 +0200

    first-task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  goodClick = ()=>{
+  this.setState(prevState => {
+    return{
+      good: prevState.good + 1,
+    }
+  })
+    };
+    neutralClick = ()=>{
+      console.log("q")
+    };
+    badClick = ()=>{
+      console.log("q")
+    };
+  render (){
+
+
+    return (
+      <>
+<h1>Please leave feedback</h1>
+<button type="button" onClick={this.goodClick}>Good</button>
+<button type="button" onClick={this.neutralClick}>Neutral</button>
+<button type="button" onClick={this.badClick}>Bad</button>
+<h2>Statistics</h2>
+<p>Good: {this.state.good} </p>
+<p>Neutral: {this.state.neutral}</p>
+<p>Bad: {this.state.bad}</p>
+      </>
+    )
+  }
+}
