19a32050a37c3a2ed43ae74ab43d418bceb03fff
YurionStyle <74459224+YurionStyle@users.noreply.github.com>
2023-05-08 19:44:20 +0200

    last fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,63 @@
 import React, { Component } from "react";
-
+import styles from "./styles.module.css";
+import Statistics from "./Statistics/Statistics";
+import Section from "./Section/Section";
+import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
+import Notification from "./Notification/Notofocation";
 export class App extends Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
-  goodClick = ()=>{
-  this.setState(prevState => {
-    return{
-      good: prevState.good + 1,
-    }
-  })
-    };
-    neutralClick = ()=>{
-      console.log("q")
-    };
-    badClick = ()=>{
-      console.log("q")
-    };
-  render (){
 
+handleFeedback = e => {
+    const { name } = e.target;
+    this.setState(prev => ({
+      [name]: prev[name] + 1,
+    }));
+  };
 
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositivePercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total > 0 ? (good / total) * 100 : 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const options = Object.keys(this.state);
+    const totalFeedback = this.countTotalFeedback();
+    const positivePercent = this.countPositivePercentage();
+
+    
     return (
-      <>
-<h1>Please leave feedback</h1>
-<button type="button" onClick={this.goodClick}>Good</button>
-<button type="button" onClick={this.neutralClick}>Neutral</button>
-<button type="button" onClick={this.badClick}>Bad</button>
-<h2>Statistics</h2>
-<p>Good: {this.state.good} </p>
-<p>Neutral: {this.state.neutral}</p>
-<p>Bad: {this.state.bad}</p>
-      </>
-    )
+      <div className={styles.container}>
+        <Section title="Please leave feedback">
+        <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.handleFeedback}
+          />
+         {totalFeedback<=0&&<Notification message="There is no feedback" />}
+        </Section>
+        {totalFeedback>0&& 
+     <Section title="Statistics">
+     <Statistics
+       good={good}
+       neutral={neutral}
+       bad={bad}
+       total={totalFeedback}
+       positivePercent={positivePercent}
+     />
+   </Section>
+      }
+        
+      </div>
+    );
   }
 }
